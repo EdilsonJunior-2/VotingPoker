483310223bf4c4979803deb1a0d0c37476a17551
Edilson <juniorse13@gmail.com>
2021-11-01 17:44:31 -0300

    Sistema de envio de mensagens (incompleto)

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -10,79 +10,51 @@ import {
   MessageSendIcon,
 } from "./styles";
 import { SendIcon } from "../../assets";
+import { Component, useState } from "react";
+import { message } from "./interfaces";
 
 const Chat: React.FC = () => {
+  const [messageInput, setMessageInput] = useState<string>("");
+  const [messageList, setMessageList] = useState<message[]>([
+    {
+      id: "0",
+      sender: "Henrique",
+      messageSent: "Mensagem",
+    },
+  ]);
+
+  const sendMessage = () => {
+    const m = messageList;
+    m.push({
+      id: m.length.toString(),
+      sender: "Pedro",
+      messageSent: messageInput,
+    });
+    setMessageList(m);
+    console.log(messageList);
+  };
+
   return (
     <ChatBox>
       <MessageList>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
-        <MessageBox name="Henrique">
-          <MessageSender name="Henrique">Henrique</MessageSender>
-          <Message>Mensagem</Message>
-        </MessageBox>
-        <MessageBox name="Pedro">
-          <MessageSender name="Pedro">Pedro</MessageSender>
-          <Message>Mensagem 2 </Message>
-        </MessageBox>
+        {messageList.map((messageData: message) => (
+          <MessageBox key={messageData.id} name={messageData.sender}>
+            <MessageSender name={messageData.sender}>
+              {messageData.sender}
+            </MessageSender>
+            <Message>{messageData.messageSent}</Message>
+          </MessageBox>
+        ))}
       </MessageList>
       <MessageInputDiv>
-        <MessageInput type="text" />
-        <MessageSendButton type="button">
+        <MessageInput
+          type="text"
+          value={messageInput}
+          onChange={(event) => {
+            setMessageInput(event.target.value);
+          }}
+        />
+        <MessageSendButton type="button" onClick={sendMessage}>
           <MessageSendIcon src={SendIcon} alt="send" />
         </MessageSendButton>
       </MessageInputDiv>
